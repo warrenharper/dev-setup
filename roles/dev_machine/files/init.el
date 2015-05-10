f13440d37c44ca5edf0c03ac832fc7121e6a433c
Warren Harper <warrenjharper@gmail.com>
2015-05-09 23:06:44 -0400

    Update with use-package library
    The use package library allows us to configure each package in a clean
    way. Also broke out the package configuration into the list/dot.el
    file. The use-package library also allows us to auto install things.

--- roles/dev_machine/files/init.el
+++ roles/dev_machine/files/init.el
@@ -1,78 +1,61 @@
- '(speedbar-show-unknown-files t)
- '(sr-speedbar-right-side nil)
- '(vc-annotate-background nil)
- '(vc-annotate-color-map (quote ((20 . "#ff9da4") (40 . "#ffc58f") (60 . "#ffeead") (80 . "#d1f1a9") (100 . "#99ffff") (120 . "#bbdaff") (140 . "#ebbbff") (160 . "#ff9da4") (180 . "#ffc58f") (200 . "#ffeead") (220 . "#d1f1a9") (240 . "#99ffff") (260 . "#bbdaff") (280 . "#ebbbff") (300 . "#ff9da4") (320 . "#ffc58f") (340 . "#ffeead") (360 . "#d1f1a9"))))
- '(vc-annotate-very-old-color nil)
-;;(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-;; '(auto-dim-other-buffers-face ((t (:background "#FFF")))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#ffffff" "#f36c60" "#8bc34a" "#fff59d" "#4dd0e1" "#b39ddb" "#81d4fa" "#263238"))
- '(auto-dim-other-buffers-mode t)
- '(custom-safe-themes
-   (quote
-    ("fc58296b4831a327aa1b11939b32755a7bd4a0679665809968132ef38d2dd4d8" "c5399a0d6397dcdcc31ee4ced0d658d41d9561b4f3e56a10ce766661349562f1" "69fccd094b1a84a70037dcf44ffefb1fec6b258c9a06e69a7bf58bf06a5b81b1" "5316a3de87d418f0bb02f73deeb6bf9ccd69ca81c46e27a50b59eea947cc3763" "764e3a6472a3a4821d929cdbd786e759fab6ef6c2081884fca45f1e1e3077d1d" "8d6fb24169d94df45422617a1dfabf15ca42a97d594d28b3584dc6db711e0e0b" "08efabe5a8f3827508634a3ceed33fa06b9daeef9c70a24218b70494acdf7855" "1c50040ec3b3480b1fec3a0e912cac1eb011c27dd16d087d61e72054685de345" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" "be50a45342f47158a8d34f54ffecc63f55dbdf66ad672c171c48e9dac56fff52" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "2a12e95e9ee6ed57592e7df12f3f028205575e9b3affdb5e6fa589421c618136" "3a91205cfbbe87a78889e7871000b73e89783dde76550dc32fd6d379a1fe70da" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "cbef37d6304f12fb789f5d80c2b75ea01465e41073c30341dc84c6c0d1eb611d" "18e89f93cbaaac214202142d910582354d36639f21f32b04718ca6425dbc82a2" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "573e46dadf8c2623256a164831cfe9e42d5c700baed1f8ecd1de0675072e23c2" "d96416845141e99d05d45b5f99ecf46458bf97654be7d2e20184c5edcda1580a" "b458d10c9ea0c8c465635b7b13e1bd23f04e6b696b1ca96cb2c4eca35a31641e" "49e5a7955b853f70d1fe751b2f896921398b273aa62f47bda961a45f80219581" "154400194a1843a22063914137c5f1ffce55f3ac369bf26589ae61d42a4540f4" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "e292ec749f785d348969926448e25790356a7ce1a8fda6e695f5e8b70bed786b" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" "1967b40b6f744f3dadc00981f92ed2871d8c583194abb5a5ad08e9e0645e5516" "4b58a8e06fa66e9b8c3ab3855ad1dcd08d236f6afe87129e19a7f01a2d4aef03" "0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "c2cfe2f1440d9ef4bfd3ef4cf15bfe35ff40e6d431264b1e24af64f145cffb11" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(fci-rule-color "#383838")
- '(org-agenda-files (quote ("~/stuff.org")))
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
-
-  
-
-
 ; Add ~/.emacs.d to load-path
 ;;(add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/elpa/")
 (let ((default-directory "~/.emacs.d/github.com-packages/"))
   (normal-top-level-add-subdirs-to-load-path))
 
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
 
-;;Add themes folder to custom themes list
-(if (boundp 'custom-theme-load-path)
-    (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-  (defvar custom-theme-load-path (cons "~/.emacs.d/themes/" '()))
-  )
 
-(setq inhibit-startup-screen t)
+;; Behaviour
+(setq inhibit-startup-screen t
+      backup-inhibited t)
+(global-auto-revert-mode t)
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
+(global-set-key (kbd "C-x r") 'rename-buffer)
+(setq ring-bell-function 'ignore)
 
 
+;; Display
 (show-paren-mode 1)
 (column-number-mode 1)
-(define-key global-map (kbd "RET") 'newline-and-indent)
-;;Get rid of tool and menu bars
+(global-linum-mode 1)
+(set-scroll-bar-mode nil)
+(set-face-attribute 'default nil :height 140)
+(set-face-attribute 'default nil :font "Source Code Pro")
+(setq-default line-spacing 3)
+(toggle-frame-fullscreen)
+
+                   
+;;;;Get rid of tool and menu bars
 (and (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (and (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;;Spacing
+(setq-default indent-tabs-mode nil
+              tab-width 4)
+
+
+;; Nice Line Wrapping
+(setq visual-line-mode 80)
+(global-visual-line-mode)
+
+
+;; IMenu
+;; Allows me to jump to functions
+(global-set-key (kbd "C-c C-f") 'imenu)
+
+;; Desktop
+(desktop-save-mode t)
+(global-set-key (kbd "C-c C-d c") 'desktop-clear)
+(global-set-key (kbd "C-c C-d s") 'desktop-save)
+(global-set-key (kbd "C-c C-d r") 'desktop-remove) 
+
+(put 'narrow-to-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+
 
 ;;Package Setup
 (require 'package)
@@ -81,179 +64,21 @@
                            ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
-;;Mac command modifier
-(setq mac-command-modifier 'control)
-;;Mac Paths
+
+
+
+
+
+
+
+
+
+
+(require 'dot)
+;;Mac Environment
 (when (memq window-system '(mac ns))
-  (progn
-    (setq-default default-directory "/Users/warren")
-    (exec-path-from-shell-initialize)
-    (exec-path-from-shell-copy-env "GOPATH")
-    (setenv "PATH" "/Users/warren/.cabal/bin:/Applications/ghc-7.8.3.app/Contents/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/go/bin:/Users/warren/.rvm/bin:/Users/warren/workbench/go/bin:/Users/warren/bin")
-    ))
+  (setq mac-command-modifier 'control))
 
 
-
-
-;;Color-themes
-;;(require 'color-theme)
-(load-theme 'zenburn t)
-
-;;Line numbering
-(global-linum-mode 1)
-
-;;Remove Scroll bar
-(set-scroll-bar-mode nil)
-
-;;Set font size
-(set-face-attribute 'default nil :height 140)
-
-;;Indention to spaces instead of tabs
-(setq-default indent-tabs-mode nil)
-
-;; Backup Policy
-(setq backup-directory-alist `(("." . "~/.saves")))
-(setq backup-by-copying t)
-(setq auto-save-list-file-prefix "~/.emacs.d/tmp/autosaves/")
-
-;;Auto Revert Mode
-(global-auto-revert-mode t)
-
-(ido-mode 1)
-(ido-ubiquitous-mode 1)
-(ido-vertical-mode 1)
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-
-;; ;; Smart Modeline
-;;  (setq sml/theme 'powerline)
-;;  (require 'smart-mode-line)
-;;  (sml/setup)
-
-;;Multiple Cursors
-(require 'multiple-cursors)
-(global-set-key (kbd "C-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c <") 'mc/skip-to-previous-like-this) 
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c >") 'mc/skip-to-next-like-this) 
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-;; Terminals
-(global-set-key (kbd "C-x t") (lambda () (interactive) (ansi-term "/usr/local/bin/bash")))
-(defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
-  (if (memq (process-status proc) '(signal exit))
-      (let ((buffer (process-buffer proc)))
-        ad-do-it
-        (kill-buffer buffer))
-    ad-do-it))
-(ad-activate 'term-sentinel)
-
-
-(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
-(global-set-key (kbd "C-x r") 'rename-buffer)
-
-
-;; Cursor
-;;(set-default 'cursor-type 'hbar)
-
-;; Power Line
-(powerline-default-theme)
-(require 'powerline)
-
-
-
-;;GO MODE
-(setq gofmt-command "goimports")
-(setq go-path (getenv "GOPATH"))
-(require 'go-mode)
-(add-hook 'before-save-hook 'gofmt-before-save)
-(message (getenv "PATH"))
-(load-file (concat go-path "/src/golang.org/x/tools/cmd/oracle/oracle.el"))
-(setq go-oracle-command (concat go-path "/bin/oracle"))
-;; Go autocomplete (needed for go-eldoc)      
-(require 'auto-complete)
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-;;(ac-config-default)
-(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-(require 'go-eldoc)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
-
-
-
-
-;; Full screen
-(toggle-frame-fullscreen)
-
-;; Org Mode
-(setq org-enforce-todo-dependencies t)
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "IN PROGRESS(p)" "|" "DONE(d)")
-        (sequence "GOAL(g)" "WAITING...(w)" "|" "COMPLETED(c)")))
-(setq org-log-done 'time)
-
-;;;; Org babel languages
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (sh . t)))
-(setq org-src-fontify-natively t)
-
-;;;; Appointments
-(setq appt-display-format 'window)
-
-;; WebMode
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-(setq web-mode-engines-alist
-      '(("go" . "\\.html\\'"))
-      )
-(setq auto-save-default nil)
-
-;; SCSS
-(setq scss-sass-command "sass --style=compressed")
-
-;; Bell
-(setq ring-bell-function 'ignore)
-
-;; Nice Line Wrapping
-(setq visual-line-mode 80)
-(global-visual-line-mode)
 (server-start)
-
-;; Ediff
-;; (setq ediff-split-window-function 'split-window-horizontally)
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-;; Set Font
-(set-face-attribute 'default nil :font "Source Code Pro")
-(setq-default line-spacing 3)
-
-;; Haskell
-(add-hook 'haskell-mode-hook 'haskell-indent-mode)
-
-
-
-;; IMenu
-;; Allows me to jump to functions
-(global-set-key (kbd "C-c C-f") 'imenu)
-
-;; Grep Edit
-(require 'wgrep)
-
-
-;; Desktop
-(desktop-save-mode t)
-(global-set-key (kbd "C-c C-d c") 'desktop-clear)
-(global-set-key (kbd "C-c C-d s") 'desktop-save)
-(global-set-key (kbd "C-c C-d r") 'desktop-remove) 
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'narrow-to-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
 
---
+++ roles/dev_machine/files/lisp/dot.el
@@ -0,0 +1,180 @@
+;; Ensure that package use-package is initialized
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(use-package exec-path-from-shell
+  ;; Used to get environment variables for mac
+  :ensure t
+  :if (memq window-system '(mac ns))
+  :init (progn
+          (setq-default default-directory "/Users/warren")
+          (exec-path-from-shell-initialize)
+          (exec-path-from-shell-copy-env "GOPATH")
+          (setenv "PATH" "/Users/warren/.cabal/bin:/Applications/ghc-7.8.3.app/Contents/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/go/bin:/Users/warren/.rvm/bin:/Users/warren/workbench/go/bin:/Users/warren/bin")
+          ))
+
+
+  (use-package zenburn-theme
+    ;; Dark theme
+    :ensure t
+    :config (load-theme 'zenburn t))
+
+  (use-package ido
+    ;; Nice mini-buffer
+    :ensure t
+    :init (ido-mode 1))
+
+  (use-package ido-ubiquitous
+    ;; ido ubiquitously
+    :ensure t
+    :config (ido-ubiquitous-mode 1))
+
+  (use-package ido-vertical-mode
+    ;; Vertical
+    :ensure
+    :config (ido-vertical-mode 1))
+
+  (use-package smex
+    ;; M-x with ido-style
+    :ensure t
+    :config (smex-initialize)
+    :bind (("M-x" . smex)))
+
+  (use-package multiple-cursors
+    ;; Multiple Cursors for Emacs.
+    :ensure t
+    :bind (("C-c <" . mc/mark-previous-like-this)
+           ("C-c <" . mc/skip-to-previous-like-this)
+           ("C->" . mc/mark-next-like-this)
+           ("C-c >" . mc/skip-to-next-like-this)
+           ("C-c C-a" . mc/mark-all-like-this)))
+
+  (use-package term
+    ;; Terminal
+    :config (progn
+              ;; Open terminal only if one doesn't exist
+              (defun wh-ansi-term ()
+                (interactive)
+                (let ((buffer (get-buffer "*ansi-term*")))
+                   (if buffer
+                       (switch-to-buffer buffer)
+                     (ansi-term (getenv "SHELL")))))
+                (define-key global-map (kbd "C-x t") 'wh-ansi-term)
+                ;; Kill terminal buffer on terminal exit
+                (defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+                  (if (memq (process-status proc) '(signal exit))
+                      (let ((buffer (process-buffer proc)))
+                        ad-do-it
+                        (kill-buffer buffer))
+                    ad-do-it))
+                (ad-activate 'term-sentinel)))
+
+  (use-package magit
+    :ensure t)
+
+  (use-package powerline
+    :ensure t
+    :init (progn
+            (powerline-default-theme)
+            (require 'powerline)))
+
+  (use-package go-mode
+    ;; GO Mode for editing go programs
+    :ensure t
+    :init (let (( gopath (getenv "GOPATH")))
+            (setq gofmt-command "goimport")
+            (require 'go-mode)
+            (add-hook 'before-save-hook 'gofmt-before-save)
+            (load-file (concat gopath "/src/golang.org/x/tools/cmd/oracle/oracle.el"))
+            (setq go-oracle-command (concat gopath "/bin/oracle"))))
+
+
+
+  (use-package auto-complete
+    :ensure t
+    :config (progn
+              (require 'auto-complete-config)
+              (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)))
+
+  (use-package go-eldoc
+    :config (progn
+              (require 'go-eldoc)
+              (add-hook 'go-mode-hook 'go-eldoc-setup)))
+
+  (use-package org
+    :config (progn
+              (setq org-enforce-todo-dependencies t)
+              (setq org-todo-keywords
+                    '((sequence "TODO(t)" "IN PROGRESS(p)" "|" "DONE(d)")
+                      (sequence "GOAL(g)" "WAITING...(w)" "|" "COMPLETED(c)")))
+              (setq org-log-done 'time)
+              (org-babel-do-load-languages
+               'org-babel-load-languages
+               '((emacs-lisp . t)
+                 (sh . t)))
+              ;; Syntax highlighting in code blocks
+              (setq org-src-fontify-natively t)
+              ;; Opens appointment reminders in current window
+              (setq appt-display-format 'window)))
+
+  (use-package web-mode
+    :ensure t
+    :init (progn
+            (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+            (setq web-mode-engines-alist
+                  '(("go" . "\\.html\\'")))))
+
+
+  (use-package scss-mode
+    :ensure t
+    :init (setq scss-sass-command "sass --style=compressed"))
+
+
+  (use-package haskell-mode
+    :ensure t
+    :init (add-hook 'haskell-mode-hook 'haskell-indent-mode))
+
+  (use-package ediff
+    :init (setq ediff-window-setup-function 'ediff-setup-windows-plain))
+
+  (use-package wgrep
+    :init (require 'wgrep))
+
+  (provide 'dot)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
